0e9dcab04e80fef3869f90a2235f333c59c375ff
Claude_Delcroix <166008956+Yurri5code@users.noreply.github.com>
2024-11-25 23:53:30 +0300

    Update main.c

--- A-хипстер вася/main.c
+++ A-хипстер вася/main.c
@@ -66,3 +66,16 @@ int main()
     printf("%d\t %d", days_of_different_socks, days_same_socks );
     return 0;
 }
+
+//c++ solution
+vector<int> quickSort(vector<int> arr) {
+    vector<int> left, equal, right;
+    for(int i = 0; i < arr.size(); i++){
+        if(arr[i] < arr[0]) left.push_back(arr[i]);
+        else if(arr[i] > arr[0]) right.push_back(arr[i]);
+        else equal.push_back(arr[i]);
+    }
+    left.insert(left.end(), equal.begin(), equal.end());
+    left.insert(left.end(), right.begin(), right.end());
+    return left;
+}
